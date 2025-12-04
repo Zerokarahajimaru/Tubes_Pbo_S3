655a1c5fbfb62a81342ab2166828ce75e1bf6603
furis <yukannahinoishi@gmail.com>
2025-12-04 21:31:55 +0700

    fix main code smell

--- src/main/java/com/vending/Main.java
+++ src/main/java/com/vending/Main.java
@@ -5,9 +5,6 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        // Clean Code: Menjalankan GUI di Event Dispatch Thread
-        SwingUtilities.invokeLater(() -> {
-            new VendingUI().setVisible(true);
-        });
+        SwingUtilities.invokeLater(() -> new VendingUI().setVisible(true));
     }
 }
